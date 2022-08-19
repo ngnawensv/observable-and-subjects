ebfc5d6f9272f64864a1fcaac7cf389136239e70
Ngnawen Samuel <ngnawens@gmail.com>
2022-08-19 23:18:34 +0400

    update oberservable component

--- src/app/observable/observable.component.ts
+++ src/app/observable/observable.component.ts
@@ -9,7 +9,16 @@ export class ObservableComponent implements OnInit {
 
   constructor() { }
 
+  students:Student[]=[{
+  id:12,
+  name:'Samuel',
+  surname:'Vermon'
+  }]
+
   ngOnInit(): void {
+  const interval$=interval(10000);
+  interval$.subscribe(val=>console.log(val));
+
   }
 
 }
