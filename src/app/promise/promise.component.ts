e0706de7c02432fa06c7662aaaaa0b05e6aa6bab
Ngnawen Samuel <ngnawens@gmail.com>
2022-08-22 13:51:00 +0400

    update promise component

--- src/app/promise/promise.component.ts
+++ src/app/promise/promise.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Student } from '../models/student';
 
 @Component({
   selector: 'app-promise',
@@ -8,28 +9,62 @@ import { Observable } from 'rxjs';
 })
 export class PromiseComponent implements OnInit {
 
-//Creating Student Promise
-  studentPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      // Handle resolve and reject in the asynchronous API
-      if (false) {
-        resolve([
-          { id: 1, name: 'Jerry' },
-          { id: 2, name: 'Elaine' },
-          { id: 3, name: 'George' },
-        ])
-      } else {
-        reject('Failed to fetch data!')
+  students: Student[] = [
+    {
+      id: 1,
+      name: 'Studant_Name 1',
+      surname: 'Student_Surname 1'
+    },{
+      id: 2,
+      name: 'Studant_Name 2',
+      surname: 'Student_Surname 2'
+    },{
+      id: 3,
+      name: 'Studant_Name 3',
+      surname: 'Student_Surname 3'
+    },{
+      id: 4,
+      name: 'Studant_Name 4',
+      surname: 'Student_Surname 4'
+    },{
+      id: 5,
+      name: 'Studant_Name 5',
+      surname: 'Student_Surname 5'
+    },{
+      id: 6,
+      name: 'Studant_Name 6',
+      surname: 'Student_Surname 6'
+    },{
+      id: 7,
+      name: 'Studant_Name 7',
+      surname: 'Student_Surname 7'
+    },{
+      id: 8,
+      name: 'Studant_Name 8',
+      surname: 'Student_Surname 8'
+    },{
+      id: 9,
+      name: 'Studant_Name 9',
+      surname: 'Student_Surname 9'
+    },{
+      id: 10,
+      name: 'Studant_Name 10',
+      surname: 'Student_Surname 10'
+    },
+  ]
 
-      }
-    }, 1000)
+//Creating Students Promise
+  studentsPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(this.students)
+    }, 1000) //emit the students liste after 1 seconde
 
   })
 
   constructor() { }
 
   ngOnInit(): void {
-    this.studentPromise.then(data => console.log("OK")).catch(err => console.log("KO"));
+    this.studentsPromise.then(students => console.log('Students List', students)).catch(err => console.log("KO"));
 
   }
 
